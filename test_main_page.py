eb1eeacc9bd822900b4b34624911694c45177450
tdgibadullin <tdgibadullin@gmail.com>
2025-06-11 02:06:15 +0300

    Implement MainPage class and refactor test using Page Object Model

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,12 @@
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    # Инициализируем Page Object, передаем в конструктор экземпляр
+    # драйвера и url адрес.
+    page = MainPage(browser, link)
+    # Открываем страницу.
+    page.open()
+    # Выполняем метод страницы — переходим на страницу логина.
+    page.go_to_login_page()
