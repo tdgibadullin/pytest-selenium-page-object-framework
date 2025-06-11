ae06d1ba1ccca0202727697472bee1e8d222042b
tdgibadullin <tdgibadullin@gmail.com>
2025-06-11 03:43:06 +0300

    Add test for login link and supporting checks in Page Object

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common import NoSuchElementException
+
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,3 +8,10 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
